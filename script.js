02b059777b47f74a8a1edc2344bafd033256d279
rk23 <reed.kinning@gmail.com>
2016-03-09 22:54:43 -0800

    latest deploy - planets replaced, start here functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var logo            = document.getElementById("logo");
+var employerTrack   = document.getElementById("employers");
+var friendsTrack    = document.getElementById("friends");
 var expandables     = document.getElementsByClassName("expandable");
 var displayArticles = document.getElementsByClassName("display-article");
 var projectImages   = document.getElementsByClassName("portfolio-pictures");
-var portfolioList   = document.getElementsByName("3")[0];
 var welcomePage     = document.getElementById('0');
+var start           = document.getElementById("start");
+var startButton     = document.getElementById("start-button");
 var articles        = document.getElementsByClassName("content-box")[0].children;
 var section1        = document.getElementsByClassName("section1");
 var section2        = document.getElementsByClassName("section2");
@@ -12,10 +15,8 @@ var section4        = document.getElementsByClassName("section4");
 var section5        = document.getElementsByClassName("section5");
 var sections        = [section1, section2, section3, section4, section5];
 var articlesOpened  = 0;
-var portfolioOpenCount  = 0;
-var portfolioArticle    = document.getElementById('3');
 
-var doTransition = function(elem, apply){
+var addOverlay = function(elem, apply){
     if(apply){
         elem.firstElementChild.style.display = "block";
     } else {
@@ -28,31 +29,23 @@ var displayWelcome = function(){
     } else {
         welcomePage.style.display = 'block';
     }
-}
-var hideAll = function(elems, sectionNumber){
-    for (var i = 0; i < elems.length; i++) {
-        for (var k = 0; k < elems[i].children.length; k++){
-            if(elems[i].children[k].firstElementChild && elems[i].children[k].firstElementChild.name.indexOf(sectionNumber) !== -1){
-                elems[i].children[k].firstElementChild.classList.remove("active");
-            }
-            if(elems[i].children[k].firstElementChild.classList.contains("expandable")){
-                elems[i].children[k].lastElementChild.style.display = "none";
-            }
-        }
-    }
-    for (i = 0; i < articles.length; i++){
-        if(articles[i].id.indexOf(sectionNumber + ".") !== -1 && articles[i].style.display == "block"){
-            articles[i].style.display = "none";
-            articlesOpened--;
-            if(sectionNumber.indexOf('3') !== -1){
-                portfolioOpenCount--;
-            }
-        }
-    }
-    displayWelcome()
 };
+var toggleHighlight = function(e){
+    var selection           = e.target;
+    var sectionNumber       = selection.name;
+    var targetSection       = document.getElementById(sectionNumber);
 
-logo.addEventListener("click", function(e){
+    if(targetSection.style.display == "block"){
+        targetSection.style.display = "none";
+        e.target.classList.remove("active")
+        articlesOpened--;
+    } else {
+        targetSection.style.display = "block";
+        e.target.classList.add("active");
+        articlesOpened++;
+    }
+}
+var hideAll = function(){
     var highlightedElements = document.getElementsByClassName("active");
     var heLength            = highlightedElements.length;
 
@@ -69,13 +62,73 @@ logo.addEventListener("click", function(e){
         articles[i].style.display = 'none';
     }
     welcomePage.style.display = "block";
+    articlesOpened = 0;
+};
+var hideSection = function(elems, sectionNumber){
+    for (var i = 0; i < elems.length; i++) {
+        for (var k = 0; k < elems[i].children.length; k++){
+            if(elems[i].children[k].firstElementChild && elems[i].children[k].firstElementChild.name.indexOf(sectionNumber) !== -1){
+                elems[i].children[k].firstElementChild.classList.remove("active");
+            }
+            if(elems[i].children[k].firstElementChild.classList.contains("expandable")){
+                elems[i].children[k].lastElementChild.style.display = "none";
+            }
+        }
+    }
+    for (i = 0; i < articles.length; i++){
+        if(articles[i].id.indexOf(sectionNumber + ".") !== -1 && articles[i].style.display == "block"){
+            articles[i].style.display = "none";
+            articlesOpened--;
+        }
+    }
+    displayWelcome()
+};
+var openTrack = function(sectionNumbers){
+    sectionNumbers.forEach(function(sectionNumber){
+        var element = document.getElementsByName(sectionNumber)[0]
+        if (element.classList.contains("expandable")){
+            element.parentElement.lastElementChild.style.display = "block";
+        }
+        element.classList.add("active")
+
+        var article = document.getElementById(sectionNumber)
+        article.style.display = "block"
+        articlesOpened++;
+    })
+};
+
+logo.addEventListener("click", function(){
+    hideAll();
+});
+
+startButton.addEventListener("click", function(e){
+    if (!e.target.classList.contains('active')){
+        hideAll();
+        toggleHighlight(e)
+        welcomePage.style.display = 'none';
+    } else {
+        hideAll();
+    }
+    displayWelcome()
+})
+
+employerTrack.addEventListener("click", function(e){
+    start.style.display = "none";
+    startButton.classList.remove("active");
+    openTrack([1, 2, 2.1, 2.2, 2.3, 3, 6])
+})
+
+friendsTrack.addEventListener("click", function(){
+    start.style.display = "none";
+    startButton.classList.remove("active");
+    openTrack([1, 1.1, 1.3, "1.3.1", "1.3.4", 1.4, 5])
 })
 
 for (var i = 0; i < expandables.length; i++){
     expandables[i].addEventListener("click", function(e){
         if (e.target.parentElement.lastElementChild.style.display == 'block'){
             e.target.parentElement.lastElementChild.style.display = "none";
-            hideAll(sections[parseInt(e.target.name) - 1], e.target.name)
+            hideSection(sections[parseInt(e.target.name) - 1], e.target.name)
         } else {
             e.target.parentElement.lastElementChild.style.display = "block";
         }
@@ -83,57 +136,15 @@ for (var i = 0; i < expandables.length; i++){
 }
 for (i = 0; i < displayArticles.length; i++){
     displayArticles[i].addEventListener("click", function(e){
-        var selection           = e.target;
-        var sectionNumber       = selection.name;
-        var targetSection       = document.getElementById(sectionNumber);
-        var flag                = false;
-        var isPortfolioOpen;
-
-        if(targetSection.style.display == "block" || (sectionNumber == "3" && selection.parentElement.lastElementChild.style.display == "none")){
-            targetSection.style.display = "none";
-            e.target.classList.remove("active")
-            articlesOpened--;
-            //if (sectionNumber.indexOf("3.") !== -1){
-            //    portfolioOpenCount--;
-            //}
-            //else if (sectionNumber === "3"){
-            //    flag = true;
-            //}
-        } else {
-            targetSection.style.display = "block";
-            e.target.classList.add("active");
-            articlesOpened++;
-            //if(alternateBackground){
-            //   //targetSection.style.backgroundColor = "#f9f9f9";
-            //}
-            //alternateBackground = !alternateBackground;
-            //if (sectionNumber.indexOf("3.") !== -1){
-            //    portfolioOpenCount++;
-            //}
-        }
-        //
-        //portfolioList.parentElement.lastElementChild.style.display == "none" ? isPortfolioOpen = false : isPortfolioOpen = true;
-        //
-        //if (portfolioArticle.style.display == "block" && sectionNumber.indexOf("3.") !== -1 && portfolioOpenCount > 0){
-        //    portfolioArticle.style.display = "none";
-        //    portfolioList.classList.remove("active");
-        //}
-        //else if (portfolioOpenCount == 0
-        //    && isPortfolioOpen
-        //    && !flag
-        //    && sectionNumber.indexOf("3.") !== -1){
-        //    portfolioArticle.style.display = "block";
-        //    portfolioList.classList.add("active");
-        //}
-
+        toggleHighlight(e)
         displayWelcome();
     })
 }
 for (i = 0; i < projectImages.length; i++) {
     projectImages[i].addEventListener("mouseenter", function(e){
-        doTransition(e.target, true)
+        addOverlay(e.target, true)
     })
     projectImages[i].addEventListener("mouseleave", function(e){
-        doTransition(e.target, false)
+        addOverlay(e.target, false)
     })
 }
